f7bb3e7830fceb3ee74ccf4e6118682c7877b1c2
Sterling <57185391+ster2068675@users.noreply.github.com>
2021-07-25 05:19:43 +0000

    Added initial aiohttp client set-up and api calls

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import os
+import aiohttp
 
 bot = commands.Bot(command_prefix="!", case_insensitive=True)
 
@@ -11,6 +12,12 @@ async def on_ready():
 @bot.command(name='hello', description="Greet the user!")
 async def hello(ctx):
   await ctx.send(f"Hello {ctx.author.name}!")
+
+@bot.command()
+async def cat(ctx):
+  async with aiohttp.ClientSession() as session:
+    req = await session.get('cat_api')
+    json = await req.json()
 
 bot.run(os.environ['TOKEN'])
 
